3e85ad6e94ccce7f804c5876fc7e3bf408676d83
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-07-30 19:43:33 -0300

    atualizado mensagem de hello

--- src/main/java/br/ufma/ecp/Lox.java
+++ src/main/java/br/ufma/ecp/Lox.java
@@ -2,6 +2,6 @@ package br.ufma.ecp;
 
 public class Lox {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello jlox!");
     }
 }
