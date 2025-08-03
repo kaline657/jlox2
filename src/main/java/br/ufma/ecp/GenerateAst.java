a1db129b22c98ab27d738adff37ac645671721d2
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-03 18:00:35 -0300

    Finaliza GenerateAst.java com métodos defineType e fechamento do writer

--- src/main/java/br/ufma/ecp/GenerateAst.java
+++ src/main/java/br/ufma/ecp/GenerateAst.java
@@ -52,4 +52,46 @@ public class GenerateAst {
     }
 
     writer.println("}");
+     writer.close();
   }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+  }
+
+  private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+    writer.println("  static class " + className + " extends " + baseName + " {");
+
+    // Constructor
+    writer.println("    " + className + "(" + fieldList + ") {");
+
+    String[] fields = fieldList.split(", ");
+    for (String field : fields) {
+      String name = field.split(" ")[1];
+      writer.println("      this." + name + " = " + name + ";");
+    }
+
+    writer.println("    }");
+
+    // Visitor pattern
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
+    // Fields
+    for (String field : fields) {
+      writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
+  }
+}
