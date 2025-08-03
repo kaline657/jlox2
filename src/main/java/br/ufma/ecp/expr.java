a8e019d89f4c76f1719d063143ed6e7b9d36fc1d
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-03 17:38:51 -0300

    Início do GenerateAst: estrutura principal e verificação de argumentos implementadas

--- src/main/java/br/ufma/ecp/expr.java
+++
@@ -1,9 +0,0 @@
-package br.ufma.ecp;
-
-  interface Visitor<R> {
-    R visitBinaryExpr(Binary expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitUnaryExpr(Unary expr);
-  }
-
