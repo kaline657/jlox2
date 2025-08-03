5ad5aabac7ea1e0478ee7ca385e60132d9e53c47
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-02 23:38:45 -0300

    feat: adiciona método scanTokens para gerar lista de tokens

---
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -0,0 +1,27 @@
+package br.ufma.ecp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static br.ufma.ecp.TokenType.*;
+
+class Scanner {
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+
+  Scanner(String source) {
+    this.source = source;
+  }
+}
+List<Token> scanTokens() {
+  while (!isAtEnd()) {
+    // Estamos no início do próximo lexema.
+    start = current;
+    scanToken();
+  }
+
+  tokens.add(new Token(EOF, "", null, line));
+  return tokens;
+}
