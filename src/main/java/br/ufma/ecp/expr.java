de8489f79457905a371a1c3ceae719c5540625a6
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-03 17:12:08 -0300

    Adiciona interface Visitor<R> com métodos de visita para cada tipo de expressão

---
+++ src/main/java/br/ufma/ecp/expr.java
@@ -0,0 +1,9 @@
+package br.ufma.ecp;
+
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+
