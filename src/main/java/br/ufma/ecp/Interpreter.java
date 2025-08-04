da2c3fce5376b6412bf37788ebad68812cf8e878
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-04 17:27:23 -0300

    Implementados visitGroupingExpr, visitUnaryExpr e visitBinaryExpr

--- src/main/java/br/ufma/ecp/Interpreter.java
+++ src/main/java/br/ufma/ecp/Interpreter.java
@@ -30,3 +30,89 @@ class Interpreter implements Expr.Visitor<Object> {
     return object.toString();
   }
 }
+@Override
+public Object visitGroupingExpr(Expr.Grouping expr) {
+  return evaluate(expr.expression);
+}
+
+@Override
+public Object visitUnaryExpr(Expr.Unary expr) {
+  Object right = evaluate(expr.right);
+
+  switch (expr.operator.type) {
+    case MINUS:
+      checkNumberOperand(expr.operator, right);
+      return -(double) right;
+    case BANG:
+      return !isTruthy(right);
+  }
+
+  // Unreachable.
+  return null;
+}
+
+@Override
+public Object visitBinaryExpr(Expr.Binary expr) {
+  Object left = evaluate(expr.left);
+  Object right = evaluate(expr.right);
+
+  switch (expr.operator.type) {
+    case BANG_EQUAL: return !isEqual(left, right);
+    case EQUAL_EQUAL: return isEqual(left, right);
+    case GREATER:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left > (double) right;
+    case GREATER_EQUAL:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left >= (double) right;
+    case LESS:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left < (double) right;
+    case LESS_EQUAL:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left <= (double) right;
+    case MINUS:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left - (double) right;
+    case PLUS:
+      if (left instanceof Double && right instanceof Double) {
+        return (double) left + (double) right;
+      }
+
+      if (left instanceof String && right instanceof String) {
+        return (String) left + (String) right;
+      }
+
+      throw new RuntimeError(expr.operator, "Operandos devem ser dois números ou duas strings.");
+    case SLASH:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left / (double) right;
+    case STAR:
+      checkNumberOperands(expr.operator, left, right);
+      return (double) left * (double) right;
+  }
+
+  // Unreachable.
+  return null;
+}
+private void checkNumberOperand(Token operator, Object operand) {
+  if (operand instanceof Double) return;
+  throw new RuntimeError(operator, "Operando deve ser um número.");
+}
+
+private void checkNumberOperands(Token operator, Object left, Object right) {
+  if (left instanceof Double && right instanceof Double) return;
+  throw new RuntimeError(operator, "Operandos devem ser números.");
+}
+
+private boolean isTruthy(Object object) {
+  if (object == null) return false;
+  if (object instanceof Boolean) return (boolean) object;
+  return true;
+}
+
+private boolean isEqual(Object a, Object b) {
+  if (a == null && b == null) return true;
+  if (a == null) return false;
+  return a.equals(b);
+}
