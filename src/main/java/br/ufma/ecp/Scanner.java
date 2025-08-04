c3f5f6d4531d8442a336f24d4ee4e0e528ccb3e8
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-08-04 13:48:20 -0300

    correções nos códigos do sacaner.java e no Astprinter.java

--- src/main/java/br/ufma/ecp/Scanner.java
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -3,17 +3,28 @@ package br.ufma.ecp;
 import java.util.ArrayList;
 import java.util.List;
 
+import static br.ufma.ecp.TokenType.BANG;
+import static br.ufma.ecp.TokenType.BANG_EQUAL;
 import static br.ufma.ecp.TokenType.COMMA;
 import static br.ufma.ecp.TokenType.DOT;
 import static br.ufma.ecp.TokenType.EOF;
+import static br.ufma.ecp.TokenType.EQUAL;
+import static br.ufma.ecp.TokenType.EQUAL_EQUAL;
+import static br.ufma.ecp.TokenType.GREATER;
+import static br.ufma.ecp.TokenType.GREATER_EQUAL;
 import static br.ufma.ecp.TokenType.LEFT_BRACE;
 import static br.ufma.ecp.TokenType.LEFT_PAREN;
+import static br.ufma.ecp.TokenType.LESS;
+import static br.ufma.ecp.TokenType.LESS_EQUAL;
 import static br.ufma.ecp.TokenType.MINUS;
+import static br.ufma.ecp.TokenType.NUMBER;
 import static br.ufma.ecp.TokenType.PLUS;
 import static br.ufma.ecp.TokenType.RIGHT_BRACE;
 import static br.ufma.ecp.TokenType.RIGHT_PAREN;
 import static br.ufma.ecp.TokenType.SEMICOLON;
+import static br.ufma.ecp.TokenType.SLASH;
 import static br.ufma.ecp.TokenType.STAR;
+import static br.ufma.ecp.TokenType.STRING;
 
 class Scanner {
   private final String source;
@@ -50,8 +61,35 @@ class Scanner {
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break;
+      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+      case '/':
+        if (match('/')) {
+          // Comentário até o fim da linha
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignorar espaços
+        break;
+      case '\n':
+        line++;
+        break;
+      case '"':
+        string();
+        break;
       default:
-        Lox.error(line, "Caractere inesperado.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Caractere inesperado.");
+        }
         break;
     }
   }
@@ -72,6 +110,56 @@ class Scanner {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "String não terminada.");
+      return;
+    }
+
+    advance(); // fecha aspas
+
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance(); // Consome o ponto
+
+      while (isDigit(peek())) advance();
+    }
+
+    String value = source.substring(start, current);
+    addToken(NUMBER, Double.parseDouble(value));
+  }
 }
-
-
